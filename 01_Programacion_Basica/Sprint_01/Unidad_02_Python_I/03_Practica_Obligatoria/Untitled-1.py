9dd7ae368f1a54adcde0f557c09effecc296cf81
vigaragranadosruben-oss <vigaragranadosruben@gmail.com>
2025-10-29 17:47:34 +0100

    actualizacion

---
+++ 01_Programacion_Basica/Sprint_01/Unidad_02_Python_I/03_Practica_Obligatoria/Untitled-1.py
@@ -0,0 +1,17 @@
+
+numeros = [1, 2, 3, 4, 5]
+
+for num in numeros:
+    print(num)
+
+# Imprime: 1, 2, 3, 4, 5 (uno por línea)
+
+for i in range(5):
+    print(f"Vuelta {i}")
+
+# Imprime:
+# Vuelta 0
+# Vuelta 1
+# Vuelta 2
+# Vuelta 3
+# Vuelta 4
